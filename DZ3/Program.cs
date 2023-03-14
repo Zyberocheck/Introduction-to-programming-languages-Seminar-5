0a088bd8b5c283d2520fc4145faa45c14cee70ea
Alexandr <sasha-bragin@mail.ru>
2023-03-15 01:56:21 +0300

    Найдите разницу между максимальным и минимальным элементов массива.

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -5,52 +5,52 @@
 
 
 
-double [] Arr(int qwe)
+int [] Arr()
 {
-    double[] randarray = new double[qwe];
+    int[] randarray = new int[6];
     for (int i = 0; i < randarray.Length; i++)
     {
-        randarray[i] = new Random().Next(1, 100);
+        randarray[i] = new Random().Next(1,100);
                
     }
     return randarray;
 }
 
-double[] arraymm =Arr();
+int[] arraymm =Arr();
 
-// int Maxi(int[] arraymm)
-// {
-//     int max = arraymm[0];
-//     for (int i = 0; i < arraymm.Length; i++)
-//     {
-//         if (arraymm[i]>max)
-//         {
-//             max=arraymm[i];
-//         }
-//     }
-//     return max;
-// }
+int Maxi(int[] arraymm)
+{
+    int max = arraymm[0];
+    for (int i = 0; i < arraymm.Length; i++)
+    {
+        if (arraymm[i]>max)
+        {
+            max=arraymm[i];
+        }
+    }
+    return max;
+}
 
-// int Mini(int[] arraymm)
-// {
-//     int min = arraymm[0];
-//     for (int i = 0; i < arraymm.Length; i++)
-//     {
-//         if (arraymm[i]<min)
-//         {
-//             min=arraymm[i];
-//         }
-//     }
-//     return min;
-// }
+int Mini(int[] arraymm)
+{
+    int min = arraymm[0];
+    for (int i = 0; i < arraymm.Length; i++)
+    {
+        if (arraymm[i]<min)
+        {
+            min=arraymm[i];
+        }
+    }
+    return min;
+}
 
-Console.WriteLine($"Наш массив: [{String.Join(", ", Arr(6))}]");
-// Console.WriteLine();
-// Console.WriteLine($"Максимальное число в нем: {String.Join(" ", Maxi(arraymm))}");
-// Console.WriteLine();
-// Console.WriteLine($"Минимальное число в нем: {String.Join(" ", Mini(arraymm))}");
-// Console.WriteLine();
-// Console.WriteLine($"Разница между максимальным и минимальным числом: {String.Join(" ", Maxi(arraymm) - Mini(arraymm))}");
+Console.WriteLine($"Наш массив: [{String.Join(", ", arraymm)}]");
+Console.WriteLine();
+Console.WriteLine($"Максимальное число в нем: {String.Join(" ", Maxi(arraymm))}");
+Console.WriteLine();
+Console.WriteLine($"Минимальное число в нем: {String.Join(" ", Mini(arraymm))}");
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным числом: {String.Join(" ", Maxi(arraymm) - Mini(arraymm))}");
 
 
 
